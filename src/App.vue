7a71e7451ac4ed355cec37d31600e833d986c716
马军 <majun@xiwicloud.com>
2022-04-25 11:43:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class=''>
+aa
+ </div>
+</template>
+
+<script>
+ export default {
+   name:'',
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+ methods:{
+
+   }
+ }
+</script>
+
+<style  lang='scss' scoped>
+
+ 
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import { resolve } from 'path'
+const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    },
+  },
+  build: {
+    sourcemap: true,
+    minify: false,
+  },
+  plugins: [
+    createVuePlugin({ jsx: true }),
+    {
+      name: 'customBlock',
+      transform(code, id) {
+        if (/type=custom/i.test(id)) {
+          const transformedAssginment = code
+            .trim()
+            .replace(/export default/, 'const __customBlock =')
+          return {
+            code: `${transformedAssginment}
+              export default function (component) {
+              const options = component.options;
+              if (!options.__customBlock) {
+                options.__customBlock = {};
+              }
+              Object.assign(options.__customBlock, __customBlock);
+            }`,
+            map: null,
+          }
+        }
+      },
+    },
+  ],
+})
+
+export default config
+
